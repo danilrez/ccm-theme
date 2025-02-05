b0c4e82d3fdc79596d007c3ea0ef1e61131ebfa4
Danil Reznichenko <61193948+danilrez@users.noreply.github.com>
2025-02-06 00:55:36 +0100

    upd index

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,7 @@
-import { flattenOptimizedTheme } from './colors';
+import {
+	flattenOptimizedTheme,
+	// flattenOptimizedTestTheme
+} from './colors';
 import { tokenColors } from './colors/tokenColors';
 import { customizations } from './customizations';
 
@@ -9,4 +12,11 @@ const publicTheme = {
 	tokenColors,
 };
 
+// Full ready theme
 console.log(JSON.stringify(publicTheme, null, 2));
+
+// Ready colors
+// console.log(JSON.stringify(flattenOptimizedTheme(customizations), null, 2));
+
+// Testing colors
+// console.log(JSON.stringify(flattenOptimizedTestTheme(customizations), null, 2));
